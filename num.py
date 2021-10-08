e663a421a25d7911d933c52dbbe07219c2a6d0cd
goyumsamuditha <goyumsamuditha@gmail.com>
2021-10-08 16:00:38 +0530

    Create the class

--- num.py
+++ num.py
@@ -1,5 +1,6 @@
-x =int(input("Enter number:"))
-a = 0
-for i in (1,x):
-    a = a + i
-print("Total",a)
+class tot:
+    x = int(input("Enter number:"))
+    a = 0
+    for i in (1, x):
+        a = a + i
+    print("Total", a)
