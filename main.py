6fbd3c8946cbadf42824820615c9368f81cbf0e3
goyumsamuditha <goyumsamuditha@gmail.com>
2021-10-08 15:44:18 +0530

    Modifed represent sentences

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def recursive(a):
     else:
         return a + recursive(a - 1)
 
-i = int(input("Enter number:"))
+i = int(input("Enter number that you want to find recursive:"))
 while i >= 0:
     print("Output:", recursive(i))
     i = int(input("Enter number:"))
