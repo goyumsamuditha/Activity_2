d638529349caa6120f8ce9eeb9c05d854c2e2df0
goyumsamuditha <goyumsamuditha@gmail.com>
2021-10-08 15:34:25 +0530

    Find the recursive of the given number

---
+++ main.py
@@ -0,0 +1,11 @@
+def recursive(a):
+    if a == 1:
+        return 1
+    else:
+        return a + recursive(a - 1)
+
+i = int(input("Enter number:"))
+while i >= 0:
+    print("Output:", recursive(i))
+    i = int(input("Enter number:"))
+print("Output:Finished:")
